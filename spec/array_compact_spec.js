a79c1504b4d0d4b90a621b81ebc9f198d0028430
Jose Antonio Pio Gil <josetonyp@latizana.com>
2016-03-01 20:25:41 +0100

    - [x] Use JSON.stringify to compare arrays in tests - [x] Adding Array Rotate function and tests

--- spec/array_compact_spec.js
+++ spec/array_compact_spec.js
@@ -4,7 +4,8 @@ var expect = chai.expect;
 
 describe('Compacting sorted array of integers', function () {
   it('Compacts an array of sorted integers', function () {
-    expect(FArray.compact([1,3,7,7,8,9,9,9,10]).toString()).to.equal([1,3,7,8,9,10].toString());
-    expect(FArray.compact([1,3,9,7,9,10,7,8,9]).toString()).to.not.equal([1,3,7,8,9,10].toString());
+    expect(JSON.stringify(FArray.compact([1,3,7,7,8,9,9,9,10]))).to.equal(JSON.stringify([1,3,7,8,9,10]));
+    expect(JSON.stringify(FArray.compact([1,3,9,7,9,10,7,8,9]))).to.not.equal(JSON.stringify([1,3,7,8,9,10]));
+    expect(JSON.stringify(FArray.compact([]))).to.equal(JSON.stringify([]));
   });
 });
---
+++ spec/rotate_array_spec.js
@@ -0,0 +1,17 @@
+var FArray = require('../lib/arrays.js');
+var chai = require("chai");
+var expect = chai.expect;
+
+describe('Array Rotation', function () {
+  it('Rotates an empty array', function () {
+    expect(JSON.stringify(FArray.rotate([], 3))).to.equal(JSON.stringify([]));
+  });
+
+  it('Rotates a single element array', function () {
+    expect(JSON.stringify(FArray.rotate([1],2))).to.equal(JSON.stringify([1]));
+  });
+
+  it('Rotates an array by given number of positions', function () {
+    expect(JSON.stringify(FArray.rotate([1,2,3,4,5,6,7,8,9], 2))).to.equal(JSON.stringify([3,4,5,6,7,8,9,1,2]));
+  });
+});
